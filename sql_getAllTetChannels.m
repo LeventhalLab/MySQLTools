f584e37c686016b33ff9ef767bc5d5fffde8a932
Matt Gaidica <matt@gaidi.ca>
2015-03-01 22:58:05 -0500

    Deprecating single tetrode masks

--- sql_getAllTetChannels.m
+++ sql_getAllTetChannels.m
@@ -1,5 +1,5 @@
-function validMask = sql_getValidTetChannels(sessionName, tetrodeID)
-% DEPRECATED
+function validMask = sql_getAllTetChannels(sessionName)
+%
 % usage: sql_getValidTetChannels(sessionName, tetrodeID, varargin)
 %
 % function to read in valid tetrode channels for a given tetrode-session.
@@ -10,8 +10,6 @@ function validMask = sql_getValidTetChannels(sessionName, tetrodeID)
 %       "RZZZZ_YYYYMMDDX" where ZZZZ is the 4 digit rat identifier,
 %       YYYYMMDD is the date, and X is a letter indicating the specific
 %       session for that date (i.e., 'a', 'b', etc.)
-%   tetrodeID - ID of the tetrode in the tetrode database for which to
-%       extract which wires are good
 %
 % OUTPUTS:
 %   validMask
@@ -19,33 +17,26 @@ function validMask = sql_getValidTetChannels(sessionName, tetrodeID)
 conn = establishConn;
 
 if isconnection(conn)
-
     % get the sessionID from the session table for the given session name
     qry = sprintf('SELECT sessionID FROM session WHERE session.sessionName= "%s"',sessionName);
     rs = fetch(exec(conn, qry));
     sessionID = rs.Data{1};
     if strcmpi(sessionID, 'no data')
-        error('sql_getValidTetChannels:invalidSession',[sessionName ' not found in session table']);
+        error('sql_getAlletChannels:invalidSession',[sessionName ' not found in session table']);
     end
     
     % read the "channelvalid" fields from the sql database for this
     % tetrode-session pair
-    qry = sprintf('SELECT ch1valid, ch2valid, ch3valid, ch4valid FROM tetrodeSession WHERE tetrodeSession.sessionID = "%d" AND tetrodeSession.tetrodeID = "%d"',...
-                  sessionID, ...
-                  tetrodeID);
+    qry = sprintf('SELECT ch1valid, ch2valid, ch3valid, ch4valid FROM tetrodeSession WHERE tetrodeSession.sessionID = "%d"',...
+                  sessionID);
     rs = fetch(exec(conn, qry));
     if strcmpi(rs.Data{1},'no data')
-        error('sql_getValidTetChannels:invalidTetrodeSession',['tetrode-session combination not found in sql database']);
+        error('sql_getAllTetChannels:invalidTetrodeSession',['tetrode-session combination not found in sql database']);
     end
-    validMask = zeros(1, length(rs.Data));   % made this general in case we have different electrodes with more channels/"tetrode" in the future
-    for i_isValid = 1 : length(rs.Data)
-        validMask(i_isValid) = rs.Data{i_isValid};
-    end
+    validMask = cell2mat(rs.Data);
+    validMask(isnan(validMask)) = 0;
     
     close(conn);
-    
 else
-    
     error('sql_createSessionsFromRaw:invalidConnection','Cannot connect to sql database');
-    
 end
