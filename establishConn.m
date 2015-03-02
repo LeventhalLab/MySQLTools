6c0ccf80675d4a4f0102eaf0afadef29ca6059e5
Matt Gaidica <matt@gaidi.ca>
2015-03-01 21:00:50 -0500

    docs

--- establishConn.m
+++ establishConn.m
@@ -3,7 +3,7 @@ function conn = establishConn()
 load('mysql.mat');
 % if this script has been compiled, make sure to include the jar
 % as a shared resource: http://www.mathworks.com/matlabcentral/answers/129715-how-do-i-connect-to-a-mysql-database-in-a-compiled-application
-% ex. -a 'C:\jdbcDriver\mysql-connector-java-5.1.21-bin.jar'
+% ex. -a 'C:\...somewhere...\mysql-connector-java-5.1.34-bin.jar'
 if ~isdeployed
     addJava(sqlJava_version);
 end
