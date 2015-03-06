df35ee9d9c73525cb29f608a073d95492f7ff5d6
Matt Gaidica <matt@gaidi.ca>
2015-03-06 14:13:11 -0500

    Consolidation of paths

---
+++ buildLeventhalPaths.m
@@ -0,0 +1,20 @@
+function leventhalPaths = buildLeventhalPaths(nasPath,sessionName,varargin)
+
+leventhalPaths = {};
+ratID = sessionName(1:5);
+leventhalPaths.rawdata = fullfile(nasPath,ratID,[ratID,'-rawdata']);
+leventhalPaths.processed = fullfile(nasPath,ratID,[ratID,'-processed']);
+leventhalPaths.session = fullfile(leventhalPaths.rawdata,sessionName,sessionName);
+
+% pass in makeFolders (ie. {'rawData'})
+if nargin == 3
+    makeFolders = varargin{1};
+    allFields = fieldnames(leventhalPaths);
+    for ii=1:length(makeFolders)
+        if ismember(makeFolders{ii},allFields)
+            if ~exist(leventhalPaths.(makeFolders{ii}),'dir')
+                mkdir(leventhalPaths.(makeFolders{ii}));
+            end
+        end
+    end
+end
