431439334d68e94223962d72f12c2339df81ae04
Matt Gaidica <matt@gaidi.ca>
2017-04-27 14:27:53 -0400

    Session electrodes update

--- v2/sqlv2_getSessionElectrodes.sql
+++ v2/sqlv2_getSessionElectrodes.sql
@@ -6,6 +6,7 @@ session_electrodes.dv,
 session_electrodes.region_id,
 session_electrodes.valid
 FROM sessions
+INNER JOIN subjects ON sessions.subject_id = subjects.id
 INNER JOIN session_electrodes ON session_electrodes.session_id = sessions.id
-INNER JOIN eib_electrodes ON eib_electrodes.id = session_electrodes.eib_electrodes_id
-WHERE sessions.name = "%s"
+INNER JOIN eib_electrodes ON eib_electrodes.site = session_electrodes.eib_site
+WHERE sessions.name = "%s" AND subjects.eib_map_id = eib_electrodes.eib_map_id
