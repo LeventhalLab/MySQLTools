7b46d9753ab3e784e309caae65384ee88521d192
Matt Gaidica <matt@gaidi.ca>
2017-06-16 15:55:06 -0400

    Added regions

--- v2/sqlv2_getSessionElectrodes.sql
+++ v2/sqlv2_getSessionElectrodes.sql
@@ -4,9 +4,12 @@ session_electrodes.ap,
 session_electrodes.ml,
 session_electrodes.dv,
 session_electrodes.region_id,
-session_electrodes.valid
+session_electrodes.valid,
+regions.name,
+regions.abbreviation
 FROM sessions
 INNER JOIN subjects ON sessions.subject_id = subjects.id
 INNER JOIN session_electrodes ON session_electrodes.session_id = sessions.id
 INNER JOIN eib_electrodes ON eib_electrodes.site = session_electrodes.eib_site
+INNER JOIN regions ON session_electrodes.region_id = regions.id
 WHERE sessions.name = "%s" AND subjects.eib_map_id = eib_electrodes.eib_map_id
